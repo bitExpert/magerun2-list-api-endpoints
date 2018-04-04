0814554d6de9b0abb4c6e5ce8c3b2b2345cebc17
Philipp Sander <p.sander@bitexpert.de>
2018-04-04 17:18:28 +0200

    render as table

--- src/Bitexpert/Magento/ListApiEndpoints/Command/ListApiEndpoints.php
+++ src/Bitexpert/Magento/ListApiEndpoints/Command/ListApiEndpoints.php
@@ -4,19 +4,29 @@ namespace Bitexpert\Magento\ListApiEndpoints\Command;
 
 use Magento\Framework\App\ObjectManager;
 use N98\Magento\Command\AbstractMagentoCommand;
+use Psr\Log\InvalidArgumentException;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ListApiEndpoints extends AbstractMagentoCommand
 {
+
+    const OPTION_OUTPUT_FORMAT = 'output-format';
 
     protected function configure()
     {
         $this
             ->setName('api:list:endpoints')
             ->setDescription('List all API endpoints')
-        ;
+            ->addOption(
+                self::OPTION_OUTPUT_FORMAT,
+                'o',
+                InputOption::VALUE_OPTIONAL,
+                'Specify the desired output format [table (default)]',
+                'table'
+            );
     }
 
     /**
@@ -28,25 +38,42 @@ class ListApiEndpoints extends AbstractMagentoCommand
     {
         $this->detectMagento($output);
         if ($this->initMagento()) {
+            $outputFormat = $input->getOption(self::OPTION_OUTPUT_FORMAT);
             /** @var \Magento\Webapi\Model\Config $serviceConfig */
             $serviceConfig = ObjectManager::getInstance()->get(\Magento\Webapi\Model\Config::class);
             $services = $serviceConfig->getServices();
 
-            //format the table
-            $table = new Table($output);
-            $table->setHeaders(array('Method', "Route", "Resources"));
-
-            foreach ($services['routes'] as $route => $methods) {
-                foreach ($methods as $method => $config) {
-                    $table->addRow([
-                        sprintf('<fg=green>%s</>', $method),
-                        sprintf('<fg=white>%s</>', $route),
-                        sprintf('<fg=red>%s</>', json_encode($config['resources']))
-                    ]);
-                }
+            switch ($outputFormat)
+            {
+                case 'table':
+                    $this->printAsTable($services, $output);
+                    break;
+                default:
+                    throw new InvalidArgumentException('Selected output-format is not a valid option');
             }
-
-            $table->render();
         }
+    }
+
+    /**
+     * @param array $services
+     * @param OutputInterface $output
+     */
+    private function printAsTable(array $services, OutputInterface $output)
+    {
+        //format the table
+        $table = new Table($output);
+        $table->setHeaders(array('Method', "Route", "Resources"));
+
+        foreach ($services['routes'] as $route => $methods) {
+            foreach ($methods as $method => $config) {
+                $table->addRow([
+                    sprintf('<fg=green>%s</>', $method),
+                    sprintf('<fg=white>%s</>', $route),
+                    sprintf('<fg=red>%s</>', json_encode($config['resources']))
+                ]);
+            }
+        }
+
+        $table->render();
     }
 }
